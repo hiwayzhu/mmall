d422e65033c72dab39fbf464a9a70e74931f1587
hiway <330852000@qq.com>
2018-11-14 23:18:07 +0800

    redispool

--- src/main/java/com/example/mmall/util/RedisPoolUtil.java
+++ src/main/java/com/example/mmall/util/RedisPoolUtil.java
@@ -5,6 +5,45 @@ import redis.clients.jedis.Jedis;
 
 public class RedisPoolUtil {
 
+    /**
+     * 设置key的有效期，单位为秒
+     * @param key
+     * @param exTime
+     * @return
+     */
+    public static Long expire(String key,int exTime){
+        Jedis jedis = null;
+        Long result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.expire(key,exTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    //exTime单位是秒
+    public static String setEx(String key,String value,int exTime){
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.setex(key,exTime,value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
     public static String set(String key,String value){
         Jedis jedis = null;
         String result = null;
@@ -14,6 +53,42 @@ public class RedisPoolUtil {
             result = jedis.set(key,value);
         } catch (Exception e) {
             e.printStackTrace();
+            RedisPool.returnBrokenResource(jedis);
+            return result;
         }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String get(String key){
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.get(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long del(String key){
+        Jedis jedis = null;
+        Long result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.del(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
     }
 }
